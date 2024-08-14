621b195cb0ea841e1b639a7e39cd7f9fc32991fd
Ashabul Kahfi <105841108523@student.unismuh.ac.id>
2024-08-14 14:46:24 +0800

    chore: Update npm dependencies and configuration files

---
+++ index.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const express = require('express');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) => {
+    res.render('home.ejs');
+});
+
+app.get('/t/:tag',(req, res) => {
+    const { tag } = req.params; 
+    res.render('tag', { tag });
+})
+
+app.get('/rand', (req, res) => {
+    const num = Math.floor(Math.random() * 10) + 1;
+    res.render('random.ejs', {random : num});
+});
+
+app.listen(8080, () => {
+    console.log('listening on host http://localhost:8080');
+});
